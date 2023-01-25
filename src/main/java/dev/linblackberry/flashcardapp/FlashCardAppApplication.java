194018a3aa8eb69268bb9f33efe4627af08d6729
Linette Mora <limora96@gmail.com>
2023-01-25 11:33:38 -0600

    Initial commit, added Spring Boot maven project

---
+++ src/main/java/dev/linblackberry/flashcardapp/FlashCardAppApplication.java
@@ -0,0 +1,13 @@
+package dev.linblackberry.flashcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/linblackberry/flashcardapp/FlashCardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.linblackberry.flashcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
