66f9c84fd0b049851367264f4c2c92eb1a70244f
Linette Mora <limora96@gmail.com>
2023-01-29 01:10:33 -0600

    Fixed Thymeleaf if statement syntax to correctly display flashcard list

--- src/main/java/dev/linblackberry/flashcardapp/repository/FlashcardRepository.java
+++ src/main/java/dev/linblackberry/flashcardapp/repository/FlashcardRepository.java
@@ -1,12 +1,11 @@
 package dev.linblackberry.flashcardapp.repository;
 
+import java.util.List;
+
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
-import org.springframework.stereotype.Repository;
 
 import dev.linblackberry.flashcardapp.model.Category;
 import dev.linblackberry.flashcardapp.model.Flashcard;
-
-import java.util.List;
 
 public interface FlashcardRepository extends CosmosRepository<Flashcard, Long> {
     List<Flashcard> findByCategory(Category category);
